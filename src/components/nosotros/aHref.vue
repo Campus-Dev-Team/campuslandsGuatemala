e0f65aeddf1c2658247e47abccac01b6d93a332c
Yave Pico <picobarrera@gmail.com>
2024-11-24 20:57:50 -0500

    feat(landing-campus): :sparkles: 6341 - maq modulo nsotros

---
+++ src/components/nosotros/aHref.vue
@@ -0,0 +1,25 @@
+<template>
+	<a :href="isRef" @click="redirect($event, isRef)">
+		<slot />
+	</a>
+</template>
+
+<script setup>
+
+const props = defineProps({
+	isRef: {
+		type: String,
+		default: '#nosotros'
+	}
+})
+
+function redirect(event, ref){
+	event.preventDefault();
+	const content = document.querySelector(ref);
+	if(!content) return
+	content.scrollIntoView({
+		behavior: 'smooth'
+	})
+}
+
+</script>
