6cd1ee98480c5248776e52c611a619b70899828e
Kevin Gonzalez <kejogo.dev@outlook.com>
2025-02-28 08:27:37 -0500

    refactor: Delete unused image files and update image paths

--- src/partials/formSponsor.vue
+++ src/partials/formSponsor.vue
@@ -2,7 +2,7 @@
 	<main class="overflow-auto h-full w-full">
 		<article class="w-full h-full">
 			<section class="flex flex-col mx-8 md:mx-32 h-full gap-y-8 pt-4">
-				<img src="/logoCampus.svg" alt="" class="object-cover w-[156px] h-[48px]">
+				<img src="src/assets/img/logos/logoCampus_v1.svg" alt="" class="object-cover w-[156px] h-[48px]">
 				<a href="/patrocina">
 					<div class="flex items-center w-fit cursor-pointer gap-x-2">
 						<img src="src/assets/img/icons/arrow-left.svg" class="size-[25px] lg:size-[16px]" />
