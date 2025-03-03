a32943ac4fff0e6ab8914b0821378a8730cbf85c
Kevin Gonzalez <kejogo.dev@outlook.com>
2025-03-03 18:18:26 -0500

    fix: linea del tiempo funcional

---
+++ src/components/nosotros/timelineData.js
@@ -0,0 +1,60 @@
+const curyear = new Date().getFullYear();
+
+
+const timelineData = [
+  {
+    year: 2017,
+    title: `La chispa del cambio`,
+    description: `Dos emprendedores colombianos llegan a Bangalore, India,
+y descubren cómo la formación tecnológica puede transformar vidas, sin saber que este viaje sería el comienzo de algo grande.`,
+  },
+  {
+    year: 2019,
+    title: `Inspiración sin fronteras`,
+    description: `En Francia y España, un innovador modelo educativo sin profesores comenzó a revolucionar la educación. Esta experiencia inspira la creación de un proyecto único en Colombia.`,
+  },
+  {
+    year: 2020,
+    title: `El despertar de una necesidad`,
+    description: `La pandemia expone la urgente demanda de talento TI en Colombia. Los emprendedores deciden dar el primer paso: formar talento local para empresas globales.`,
+  },
+  {
+    year: 2021,
+    title: `Nace Campuslands`,
+    description: `Con un espacio limitado dedicado a la formación de 40 jóvenes talentos. A pesar de los desafíos, descubren una verdad clave: el mundo necesita más programadores, y ellos están listos para liderar la transformación.`,
+  },
+  {
+    year: 2022,
+    title: `Apuesta por la expansión`,
+    description: `El riesgo se hace realidad: se triplica el espacio y se recibe a 120 jóvenes. La deserción baja al 40% y el propósito se clarifica: formar el talento del mañana, hoy.`,
+  },
+  {
+    year: 2022,
+    title: `El mundo pone los ojos en Bucaramanga`,
+    description: `Empresarios de EE.UU. y México quedan impresionados por el impacto de Campuslands y deciden invertir, creando el primer centro de movilidad social en Colombia y Latinoamérica ubicado en Zona Franca de Santander.`,
+  },
+  {
+    year: 2023,
+    title: ` El sueño se materializa`,
+    description: `Campuslands inaugura su campus 100% presencial, con formación intensiva en programación, inglés y habilidades blandas, transformando a los jóvenes en profesionales listos para el mundo laboral.`,
+  },
+  {
+    year: 2024,
+    title: `Expansión Nacional: Nuestro talento
+a  nivel nacional`,
+    description: `Campuslands amplía su presencia en todo el país, llevando su modelo de formación y reclutamiento a diversas regiones, acercando el talento tecnológico a más empresas y estudiantes.`,
+  },
+  {
+    year: 2025,
+    title: `Expansión Internacional: Campuslands Guatemala`,
+    description: `Campuslands extiende sus fronteras y llega a Guatemala, llevando su innovador modelo educativo a más jóvenes talentosos. Con un enfoque en la formación intensiva en programación, inglés y habilidades blandas, Campuslands Guatemala se convierte en un faro de oportunidades, transformando vidas y potenciando el talento tecnológico en la región.`,
+  },
+  {
+    year: "Hoy",
+    title: `La historia continua`,
+    description: `La historia continua`,
+    description: `Con presencia internacional e innumerables desarrolladores formados, Campuslands se consolida como referente en la transformación del talento tecnológico en Latinoamérica. El futuro se escribe con código, y nuestros Campers son los autores de esta nueva era digital.`,
+  },
+];
+
+export default timelineData;
