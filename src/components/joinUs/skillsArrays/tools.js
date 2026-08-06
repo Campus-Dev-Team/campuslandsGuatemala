147993a9df0cf394ddd11619fede19c1ddbf606f
Andre Lopez <andrelopez012@gmail.com>
2026-08-06 06:23:13 -0600

    feat(camper): reemplaza jira y slack por docker y dbeaver

--- src/components/joinUs/skillsArrays/tools.js
+++ src/components/joinUs/skillsArrays/tools.js
@@ -3,9 +3,9 @@ const path = "/img/campers/tools/";
 const tools = [
     {name: "github", asset: path + `github.svg`},
     {name: "gpt", asset: path + `gpt.svg`},
-    {name: "jira", asset: path + `Jira.svg`},
+    {name: "Docker", asset: path + `docker.png`},
     {name: "linux", asset: path + `linux.svg`},
-    {name: "slack", asset: path + `slack.svg`},
+    {name: "DBeaver", asset: path + `dbeaver.png`},
     {name: "vscode", asset: path + `vsCode.svg`}
 ]
 
